fa41c827376c050b44b45c2e1740cf2040c178f7
ozcan <28202665+ozcanseker@users.noreply.github.com>
2019-09-23 15:17:23 +0200

    Updates

---
+++ precache-manifest.eef84e85cbf9638b6924cb6f9cc790e1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1788d0f56e86a5f6aa54dbd792673a9e",
+    "url": "/SolidBeerCounter/index.html"
+  },
+  {
+    "revision": "776eb069c7f1804209a1",
+    "url": "/SolidBeerCounter/static/css/main.77d3f2ec.chunk.css"
+  },
+  {
+    "revision": "6944179ceac0db48f019",
+    "url": "/SolidBeerCounter/static/js/2.9ea927f2.chunk.js"
+  },
+  {
+    "revision": "776eb069c7f1804209a1",
+    "url": "/SolidBeerCounter/static/js/main.e45b033a.chunk.js"
+  },
+  {
+    "revision": "c446efb2cb999242ad24",
+    "url": "/SolidBeerCounter/static/js/runtime~main.df1e5a72.js"
+  }
+]);
